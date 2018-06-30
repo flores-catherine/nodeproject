eb74641d67f4c4fb4c8c2799288160b1b9d02871
Catherine <catherine.g.flores@gmail.com>
2018-06-30 13:05:23 -0600

    stupid cookies

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 //middleware that goes with parsing stuff.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
 
 //middleware for static files -- set static path
 app.use(express.static(path.join(__dirname, 'public')));
