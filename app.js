60132b9c49f06dbdd419cafcf56a8860ca2fa92f
Catherine <catherine.g.flores@gmail.com>
2018-06-30 11:58:57 -0600

    worked on ecommerce site

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+const userModel = require("./model/user-model.js")
+var urlEncodedParser = bodyParser.urlencoded({ extended: false});
+
+var app = express();
+
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middleware that goes with parsing stuff.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+//middleware for static files -- set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+          var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+        
+        while(namespace.length) {
+            formParam += '[]' + namespace.shift() +']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value : value
+        };
+    }
+}));
+
+app.use(flash());
+
+//global vars
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+app.get('/render-login', function(req,res){
+    res.render('pages/login');
+});
+
+app.get('/render-register', function(req,res){
+    res.render('pages/registration');
+});
+
+app.get('/render-cart', function(req,res){
+    res.render('pages/cart');
+});
+
+app.post('/handleRegistration', userModel.handleRegistration);
+
+app.post('/handleLoginRequest', userModel.handleLogin);
+
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000!'));
---
+++ model/user-model.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var passport = require('passport');
+
+//Get db URL from process environment variable
+const dbURL = process.env.DATABASE_URL;
+
+const { Pool } = require('pg')
+const pool = new Pool({
+    connectionString: dbURL,
+    ssl: true
+});
+
+var bcrypt = require('bcrypt');
+
+function handleRegistration(req, res){
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var password = req.body.password;
+    bcrypt.hash('password', 10, function(err, hash) {
+        pool.query('INSERT INTO users(firstname, lastname, useremail, userpassword) VALUES ($1, $2, $3, $4)', [firstName, lastName, email, hash], (err, results) => {
+            if (err) {
+                console.log(err);
+            } else{
+                console.log(results);
+                res.render('pages/login');
+            }
+        });
+    });
+}
+
+function handleLogin(req, res) {
+    passport.authenticate('local', {successRedirect:'/', failureRedirect: 'pages/login', fauilureFlash: true}), function(req, res)
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log(email); 
+    console.log(password);
+    pool.query('SELECT userpassword FROM users WHERE useremail = $1', [email], (err, results) => {
+        if (err) {
+            console.log(err);
+        } else{
+            console.log(results.rows[0]);
+        }
+        
+        const hash = results.rows[0];
+        
+        bcrypt.compare(password, hash, function(err, response) {
+//            console.log(password);
+//            console.log(hash);
+            if (response == true) {
+                console.log("Have some Bacon!");
+            }else{
+                console.log("No bacon for you.");
+            }
+        });
+    
+    });
+    
+    
+    res.render('pages/index', {message:'Success'});
+}
+module.exports = {
+    handleLogin: handleLogin,
+    handleRegistration: handleRegistration
+};
